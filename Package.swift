50acdd62a5e22c5feb00ad6ae2b0275e7527a5f0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-18 20:17:03 +0100

    Migrate tests to nested package pattern

--- Package.swift
+++ Package.swift
@@ -9,27 +9,23 @@ let package = Package(
         .iOS(.v26),
         .tvOS(.v26),
         .watchOS(.v26),
-        .visionOS(.v26),
+        .visionOS(.v26)
     ],
     products: [
-        .library(name: "XML", targets: ["XML"]),
+        .library(name: "XML", targets: ["XML"])
     ],
     dependencies: [
         .package(path: "../../swift-standards/swift-w3c-xml"),
-        .package(path: "../swift-async"),
+        .package(path: "../swift-async")
     ],
     targets: [
         .target(
             name: "XML",
             dependencies: [
                 .product(name: "W3C XML", package: "swift-w3c-xml"),
-                .product(name: "Async", package: "swift-async"),
+                .product(name: "Async", package: "swift-async")
             ]
-        ),
-        .testTarget(
-            name: "XML Tests",
-            dependencies: ["XML"]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
